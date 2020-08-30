373c8d5ea0283c6f81b7c6c58ac9449e51bbb8a0
Elio Suarez <Esuarez.gcc@gmail.com>
2020-08-30 18:04:42 +0100

    chore: added amplify and getRainbowColors lambda

--- App.tsx
+++ App.tsx
@@ -3,6 +3,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import RainbowScreen from './src/screens/rainbow'
 
+import Amplify from 'aws-amplify'
+import awsConfig from './aws-exports'
+
+Amplify.configure(awsConfig)
+
+
 export default function App() {
   return (
       <RainbowScreen/>
